b0c7c37173aa97dd97fbdefeccf22634638dcf39
matheus.rocco <roccomatheus0@gmail.com>
2025-03-21 08:20:32 -0300

    add Bea's work due bugs

---
+++ src/main/java/com/AgiBank/model/FatorPrevidenciario.java
@@ -0,0 +1,14 @@
+﻿package com.AgiBank.model;
+
+public class FatorPrevidenciario {
+    private int anosContribuidos;
+    private double EXPECTATIVA_DE_VIDA = 76.4;
+    private double ALIQUOTA = 0.31;
+
+    public FatorPrevidenciario(ContribuicaoTotais contribuicaoTotais) {
+        this.anosContribuidos = contribuicaoTotais.getAnosContribuicao();
+    }
+    public double calcularFatorPrevidenciario() {
+        return (anosContribuidos * ALIQUOTA) / (EXPECTATIVA_DE_VIDA * 100);
+    }
+}
